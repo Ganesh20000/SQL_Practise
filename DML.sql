3023c7bb8aaafe00590af01980690c724c888b7f
Ganesh20000 <ganeshkarli19@gmail.com>
2026-01-27 22:38:40 +0530

    last commit of the day

--- DML.sql
+++ DML.sql
@@ -47,6 +47,50 @@ select model,battery_capacity,os from phone;
 
 -- as keyword
 
-select os as 'operating_system',battery_capacity as 'MAH' FROM PHONE
+select os as 'operating_system',battery_capacity as 'MAH' FROM PHONE;
 
--- PPI SQ
+-- PPI SQRT FORMULA
+
+-- Select model,round(sqrt(resolution_width*resolution_width+resolution_ aheight*resolution_height))  as 'ppi' from phone
+
+
+
+select * from phone;
+
+select model ,
+sqrt(resolution_width*resolution_width+
+resolution_height*resolution_height)
+/screen_size as 'ppi'
+ from phone;
+
+
+SELECT
+  model,
+  sqrt(
+    resolution_width * resolution_width +
+    resolution_height * resolution_height
+  ) / screen_size AS 'ppi'
+FROM phone;
+
+
+-- RATING
+
+
+SELECT MODEL,RATING/10 FROM PHONE;
+
+-- distinct
+select distinct(brand_name) as 'brand_name' from phone;
+
+
+-- distinct 
+select distinct(processor_brand) as 'processor' from phone;
+
+
+select distinct(os) as 'operating' from phone;
+
+-- combination of both
+
+select distinct brand_name,processor_brand from phone
+
+
+-- filter row using where clause 
