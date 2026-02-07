3cf1df1f49ea117edc7e19ce809961575088e388
Ganesh20000 <ganeshkarli19@gmail.com>
2026-02-07 15:39:37 +0530

    doing subquery

--- Subquery.sql
+++ Subquery.sql
@@ -138,4 +138,54 @@ select director  from movies
 GROUP BY director
 order by sum(gross) desc limit 0,3;
 
+-- find the movies that made by top 3 director in term of gross income 
+
+with directors as (select director from movies 
+group by director
+order by sum(gross) desc
+limit 3)
+
+select * from movies
+where director in (select * from directors);
+
+
+USE SUBQUERY;
+
+
+
+WITH TOP_DIRECTOR AS (
+    SELECT DIRECTOR 
+    FROM MOVIES 
+    GROUP BY DIRECTOR
+    ORDER BY SUM(GROSS) DESC
+    LIMIT 3
+) 
+
+SELECT * FROM MOVIES 
+WHERE DIRECTOR IN (SELECT * FROM TOP_DIRECTOR);
+
+show tables;
+
+
+-- find all movies of those actors whose filmography avg rating >8.5(take 25000 votes as cutoff) 
+
+-- normal way first 
+select * from movies 
+where star in ( select star  from movies
+where votes >25000
+group by star
+having avg(score) >8.5 );
+
+
+-- with cte same answer 
+
+with top_actors as (
+select star from movies
+where votes >25000
+group by star
+having avg(score) >8.5)
+
+select * from movies
+where star in ( select * from top_actors);
+
 
